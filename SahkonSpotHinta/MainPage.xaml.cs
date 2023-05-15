20021278d74700e99aacae98b6047a72c7ffe1d0
Olli <olli.p.mononen@gmail.com>
2023-05-15 11:43:43 +0300

    Kommentteja

--- SahkonSpotHinta/MainPage.xaml.cs
+++ SahkonSpotHinta/MainPage.xaml.cs
@@ -9,16 +9,18 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+	// Alla oleva koodi on .net mauin omaa esimerkkiä. Pidän sen et voi tutustua.
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	//private void OnCounterClicked(object sender, EventArgs e)
+	//{
+	//	count++;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+	//	if (count == 1)
+	//		CounterBtn.Text = $"Clicked {count} time";
+	//	else
+	//		CounterBtn.Text = $"Clicked {count} times";
+
+	//	SemanticScreenReader.Announce(CounterBtn.Text);
+	//}
 }
 
