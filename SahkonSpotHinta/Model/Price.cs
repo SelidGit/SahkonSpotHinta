6fb7572fe85a223c5f79ab34224e7fe1faac19d5
Olli <olli.p.mononen@gmail.com>
2023-05-16 11:14:52 +0300

    Setting lisäys ja model spothinnalle

---
+++ SahkonSpotHinta/Model/Price.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SahkonSpotHinta.Model
+{
+    public class Price
+    {
+        public double price { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+    public class Root
+    {
+        public List<Price> prices { get; set; }
+    }
+
+}
