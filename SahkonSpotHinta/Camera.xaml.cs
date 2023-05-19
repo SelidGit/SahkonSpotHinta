faff94878bf6fb006322f59b952741e11a6f88bf
Olli <olli.p.mononen@gmail.com>
2023-05-19 15:36:58 +0300

    Kameralla voi ottaa kuvan

--- SahkonSpotHinta/Camera.xaml.cs
+++ SahkonSpotHinta/Camera.xaml.cs
@@ -11,9 +11,15 @@ public partial class Camera : ContentPage
         cameraView.Camera = cameraView.Cameras.First();
         MainThread.BeginInvokeOnMainThread(async () =>
         {
+            //jos on probleemaa samsungien yms muiden kans niin kannattaa kokeilla ensin StopCameraAsync
+            // await cameraView.StopCameraAsync();
             await cameraView.StartCameraAsync();
 
 
         });
+    }
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        myImage.Source = cameraView.GetSnapShot();
     }
 }
